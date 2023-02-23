f1d3cb7e4b04108a1181dfdc7422647b0c65cfa9
Michael Granberry <michaelgranberryii@gmail.com>
2023-02-22 16:41:02 -0800

    added procedure 3, 4, and 5

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// Michael Granberry
+// COMP 322L - Lab 2
+// Spring 2023
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -5,7 +9,6 @@
 // declare global variables including a table structure to hold scheduling information
 struct node {
     int id, arrival, total_cpu, total_remaining, done, start_time, already_started, end_time, turnaround_time;
-
 } *table_ptr = NULL;
 typedef struct node table_type;
 
@@ -19,18 +22,19 @@ int num_processes;
 void PROCEDURE_TO_PRINT_THE_CONTENTS_OF_THE_SCHEDULING_TABLE() {
 // declare local variables
 // print table header
-    printf("ID\tArrival Total\tStart\tEnd\tTurnaround");
-    printf("--------------------------------------------------");
+    printf("ID\t\tArrival\tTotal\tStart\tEnd\t\tTurnaround\n");
+    printf("--------------------------------------------------\n");
 // for each process
     for(int i = 0; i < num_processes; i++) {
         // print the contents (id, arrival time, total_cycles) of each field of the table's index
-        printf("%d\t%d\t%d\t", table_ptr[i].id, table_ptr[i].arrival, table_ptr[i].total_cpu);
+        printf("p%d\t\t%d\t\t%d\t\t", table_ptr[i].id, table_ptr[i].arrival, table_ptr[i].total_cpu);
         // if process has been scheduled ("done" field is 1, print other contents (start time, end time, turnaround time)
         if(table_ptr[i].done == 1) {
-            printf("%d\t%d\t%d\t", table_ptr[i].start_time, table_ptr[i].end_time, table_ptr[i].turnaround_time);
+            printf("%d\t\t%d\t\t%d\t", table_ptr[i].start_time, table_ptr[i].end_time, table_ptr[i].turnaround_time);
         }
+        printf("\n");
     }
-    printf("\n");
+
 return;
 }
 
@@ -46,20 +50,24 @@ void PROCEDURE_FOR_OPTION_1() {
     // for each process
     for(int i = 0; i < num_processes; i++) {
         // prompt for arrival time, and total cycle time
+        table_ptr[i].id = i;
         printf("Enter arrival cycle for process p%d: ", i);
         scanf("%d",&table_ptr[i].arrival);
         printf("Enter total cycles for process p%d ", i);
         scanf("%d",&table_ptr[i].total_cpu);
     }
+
     // print contents of table
+    PROCEDURE_TO_PRINT_THE_CONTENTS_OF_THE_SCHEDULING_TABLE();
     return;
 }
 
 
 //*************************************************************
+//FIFO algorithm
 void PROCEDURE_FOR_OPTION_2() {
-    // declare (and initilize when appropriate) local variables
-    int number_of_done_processes;
+    // declare (and initialize when appropriate) local variables
+    int number_of_done_processes = 0;
     int earliest_arrival;
     int earliest_index;
     int current_cycle = 0;
@@ -69,7 +77,7 @@ void PROCEDURE_FOR_OPTION_2() {
     }
     // while there are still processes to schedule
     while (number_of_done_processes < num_processes) {
-        // initilize the earliest arrival time to INT_MAX (largest integer value)
+        // initialize the earliest arrival time to INT_MAX (the largest integer value)
         earliest_arrival = INT_MAX;
         // for each process not yet scheduled
         for (int i = 0; i < num_processes; i++){
@@ -81,67 +89,207 @@ void PROCEDURE_FOR_OPTION_2() {
                 } //if
             } //if
         }//for
-        // set start time, end time, turnaround time, done fields for unscheduled process with earliest arrival time
+        // set start time, end time, turnaround time, done fields for unscheduled process with the earliest arrival time
         table_ptr[earliest_index].start_time = max(table_ptr[earliest_index].arrival, current_cycle);
         table_ptr[earliest_index].end_time = table_ptr[earliest_index].start_time + table_ptr[earliest_index].total_cpu;
         table_ptr[earliest_index].turnaround_time = table_ptr[earliest_index].end_time - table_ptr[earliest_index].arrival;
+
         // update current cycle time and increment number of processes scheduled
         current_cycle = table_ptr[earliest_index].end_time;
         table_ptr[earliest_index].done = 1;
         number_of_done_processes++;
     }// while
+
     // print contents of table
+    PROCEDURE_TO_PRINT_THE_CONTENTS_OF_THE_SCHEDULING_TABLE();
     return;
 }
 
 
 //*************************************************************
+//SJF algorithm
 void PROCEDURE_FOR_OPTION_3() {
-    // declare (and initilize when appropriate) local variables
+    // declare (and initialize when appropriate) local variables
+    int number_of_done_processes = 0;
+    int lowest_index;
+    int lowest_total_cycle_time;
+    int current_cycle = 0;
+    int at_least_one; //at least one process is ready
+
     // for each process, reset "done" field to 0
+    for( int i = 0; i < num_processes; i++) {
+        table_ptr[i].done = 0;
+    }
+
     // while there are still processes to schedule
-    // initilize the lowest total cycle time to INT_MAX (the largest integer value)
-    // for each process not yet scheduled
-    // check if process has lower total cycle time than current lowest and has arrival time less than current cycle time and update
-    // set start time, end time, turnaround time, done fields for unscheduled process with lowest (and available) total cycle time
-    // update current cycle time and increment number of processes scheduled
+    while (number_of_done_processes < num_processes) {
+        // initialize the lowest total cycle time to INT_MAX (the largest integer value)
+        lowest_total_cycle_time = INT_MAX;
+        at_least_one = 0;
+        // for each process not yet scheduled
+        for (int i = 0; i < num_processes; i++) {
+            if (table_ptr[i].done == 0) {
+                // check if process has lower total cycle time than current lowest and has arrival time less than current cycle time and update
+                if ((table_ptr[i].total_cpu < lowest_total_cycle_time) && (table_ptr[i].arrival <= current_cycle)) {
+                    at_least_one = 1;
+                    lowest_total_cycle_time = table_ptr[i].total_cpu;
+                    lowest_index = i;
+                }//if
+            }//if
+
+        }//for
+
+        if (at_least_one == 0) {
+            current_cycle++;
+        }
+        else {
+            // set start time, end time, turnaround time, done fields for unscheduled process with lowest (and available) total cycle time
+            table_ptr[lowest_index].start_time = max(table_ptr[lowest_index].arrival, current_cycle);
+            table_ptr[lowest_index].end_time = table_ptr[lowest_index].start_time + table_ptr[lowest_index].total_cpu;
+            table_ptr[lowest_index].turnaround_time = table_ptr[lowest_index].end_time - table_ptr[lowest_index].arrival;
+
+            // update current cycle time and increment number of processes scheduled
+            current_cycle = table_ptr[lowest_index].end_time;
+            table_ptr[lowest_index].done = 1;
+            number_of_done_processes++;
+        }
+    }//while
+
     // print contents of table
+    PROCEDURE_TO_PRINT_THE_CONTENTS_OF_THE_SCHEDULING_TABLE();
     return;
 }
 
 
 //*************************************************************
+//SRT algorithm
 void PROCEDURE_FOR_OPTION_4() {
-    // declare (and initilize when appropriate) local variables
-    // for each process, reset "done", "total_remaining" and "already_started" fields to 0
-    // while there are still processes to schedule
-    // initilize the lowest total remaining time to INT_MAX (largest integer value)
-    // for each process not yet scheduled
-    // check if process has lower total remaining time than current lowest and has arrival time less than current cycle time and update
+
     // check if process already partially-scheduled
     // if so, set "start time", "already_started" fields of process with lowest (and available) total remaining cycle time
     // set end time, turnaround time of process with lowest (and available) total remaining cycle time
     // decrement total remaining time of process with lowest (and available) total remaining cycle time
     // if remaining time is 0, set done field to 1, increment cycle time and number of scheduled processes
+
+    // declare (and initialize when appropriate) local variables
+    int number_of_done_processes = 0;
+    int lowest_index;
+    int lowest_remaining;
+    int current_cycle = 0;
+    int at_least_one; //at least one process is ready
+
+    // for each process, reset "done", "total_remaining" and "already_started" fields to 0
+    for( int i = 0; i < num_processes; i++) {
+        table_ptr[i].done = 0;
+        table_ptr[i].already_started = 0;
+        table_ptr[i].total_remaining = table_ptr[i].total_cpu;
+    }
+
+    // while there are still processes to schedule
+    while (number_of_done_processes < num_processes) {
+        // initialize the lowest total remaining time to INT_MAX (the largest integer value)
+        lowest_remaining = INT_MAX;
+        at_least_one = 0;
+        // for each process not yet scheduled
+        for (int i = 0; i < num_processes; i++) {
+            if (table_ptr[i].done == 0) {
+                // check if process has lower total remaining time than current lowest and has arrival time less than current cycle time and update
+                if ((table_ptr[i].total_remaining < lowest_remaining) && (table_ptr[i].arrival <= current_cycle)) {
+                    at_least_one = 1;
+                    lowest_remaining = table_ptr[i].total_remaining;
+                    lowest_index = i;
+                }//if
+            }//if
+
+        }//for
+
+        if (at_least_one == 0) {
+            current_cycle++;
+        }
+        else {
+            // set start time, end time, turnaround time, done fields for unscheduled process with lowest (and available) total cycle time
+            if (table_ptr[lowest_index].already_started == 0) {
+                table_ptr[lowest_index].start_time = max(table_ptr[lowest_index].arrival, current_cycle);
+                table_ptr[lowest_index].already_started = 1;
+            }
+
+            table_ptr[lowest_index].end_time = current_cycle + 1;
+            table_ptr[lowest_index].turnaround_time = table_ptr[lowest_index].end_time - table_ptr[lowest_index].arrival;
+            table_ptr[lowest_index].total_remaining--;
+
+            // update current cycle time and increment number of processes scheduled
+            current_cycle++;
+            if (table_ptr[lowest_index].total_remaining == 0) {
+                table_ptr[lowest_index].done = 1;
+                number_of_done_processes++;
+            }//if
+        }//else
+    }//while
+
     // print contents of table
-    return;
+    PROCEDURE_TO_PRINT_THE_CONTENTS_OF_THE_SCHEDULING_TABLE();
 }
 
 
 //*************************************************************
+//Free memory
 void PROCEDURE_FOR_OPTION_5() {
     // free the schedule table if not NULL
+    if (table_ptr != NULL) {
+        free(table_ptr);
+    }
+    table_ptr = NULL;
 return;
 }
 
+//*************************************************************
+void menu () {
+    // declare local vars
+    int sel = 0;
+    do {
+        // print menu of options
+        printf("\nBatch scheduling\n");
+        printf("----------------\n");
+        printf("1) Enter parameters\n");
+        printf("2) Schedule processes with FIFO algorithm\n");
+        printf("3) Schedule processes with SJF algorithm\n");
+        printf("4) Schedule processes with SRT algorithm\n");
+        printf("5) Quit and free memory\n");
+        // prompts for menu selection
+        printf("Enter a selection: ");
+        scanf("%d", &sel);
+        // while user has not chosen to quit
+        // call appropriate procedure based on choice--use switch statement or series of if, else if, else statements
+        switch (sel) {
+            case 1:
+                printf("\nEnter selection: %d\n", sel);
+                PROCEDURE_FOR_OPTION_1();
+                break;
+            case 2:
+                printf("\nEnter selection: %d\n", sel);
+                PROCEDURE_FOR_OPTION_2();
+                break;
+            case 3:
+                printf("\nEnter selection: %d\n", sel);
+                PROCEDURE_FOR_OPTION_3();
+                break;
+            case 4:
+                printf("\nEnter selection: %d\n", sel);
+                PROCEDURE_FOR_OPTION_4();
+                break;
+            case 5:
+                printf("\nEnter selection: %d\n", sel);
+                PROCEDURE_FOR_OPTION_5();
+                printf("Quitting program...\n");
+                break;
+        }
+    } while (sel != 5); // while loop
+    return;
+}
 
 //*************************************************************
 int main() {
-    // declare local vars
-    // while user has not chosen to quit
-    // print menu of options
-    // prompt for menu selection
-    // call appropriate procedure based on choice--use switch statement or series of if, else if, else statements
-    // while loop
+    // menu selection
+    menu();
     return 1; // indicates success
 } // end of procedure
